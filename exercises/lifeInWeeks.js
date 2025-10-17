3620980eb9b14117c395be870e60b954bf5ac3dd
Feyistu Endale <fayistuendale@gmail.com>
2025-10-17 23:37:44 +0300

    Create lifeInWeeks.js

---
+++ exercises/lifeInWeeks.js
@@ -0,0 +1,7 @@
+function lifeInWeeks(age) {
+    var x = (90 - age) * 365;
+    var y = (90 - age) * 52;
+    var z = (90 - age) * 12;
+    console.log("You have " + x + " days, " + y + " weeks and " + z + " months left.");
+}
+lifeInWeeks(56);
